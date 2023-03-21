0ce9c39358e61bf91bd413c1fcaa8285eb7c047d
JorgeRomero01 <jrome140@fiu.edu>
2023-03-20 22:15:42 -0400

    ...

--- ThymeleafCode1/src/main/java/com/luv2code/ThymeleafCode1/Controller/EmployeeController.java
+++ ThymeleafCode1/src/main/java/com/luv2code/ThymeleafCode1/Controller/EmployeeController.java
@@ -7,12 +7,14 @@ import java.util.List;
 
 import javax.annotation.PostConstruct;
 
+import org.omg.CORBA.PUBLIC_MEMBER;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/employees")
@@ -41,6 +43,16 @@ public class EmployeeController {
 		
 		theModel.addAttribute("employee", theEmployee);
 		
+		return "employees/employee-form";
+	}
+	
+	@GetMapping("/showFormForUpdate")
+	public String showFormForUpdate(@RequestParam("employeeId") int theId, Model theModel) {
+		//get the employee from the service
+		Employee theEmployee = employeeService.findById(theId);
+		//set employee in the model
+		
+		//send over to our form
 		return "employees/employee-form";
 	}
 	
